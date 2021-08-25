97b8185ab0419feae4a3034d8272853c3924ffe9
SoadTech <fernandojoseropero@gmail.com>
2021-08-24 19:51:36 -0500

    chore: changing with to border buttonm

--- src/components/SearchBox/styles.js
+++ src/components/SearchBox/styles.js
@@ -3,7 +3,7 @@ import colors from "../../utils/colors";
 
 export const styles = StyleSheet.create({
     container: {
-        borderBottomWidth: 2,
+        borderBottomWidth: 1,
         borderBottomColor: colors.black
     }
 })
